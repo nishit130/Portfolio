350a111ed37757a92eafd3f2d55c0dd382d5113c
nis130 <nishitlimbani138@gmail.com>
2019-06-29 13:07:06 +0530

    m

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// var i =0;
+// var txt = 'I Am Nishit Patel';
+// function typeWriter(){
+//  if(i < txt.length) {
+    
+
+// document.getElementById('demo').innerHTML += txt.charAt(i);
+// i++;
+// setTimeout(typeWriter , 500)
+
+// }
+// }
+// typeWriter();
